8d465852c2515c7a9bd2d7dd31c9a95285b0c44f
michal-fre <mike@mbp.local>
2015-09-23 10:17:40 +0200

    use the mintier

--- bash-scripts-for-pdf-generation/scripts/createpdf.sh
+++ bash-scripts-for-pdf-generation/scripts/createpdf.sh
@@ -19,26 +19,13 @@ xelatex minitex-multi.tex
 xelatex minitex-multi.tex
 xelatex minitex-multi.tex
 
-xelatex rpb-test-jur.tex
-xelatex rpb-test-jur.tex
-xelatex rpb-test-jur.tex
-xelatex rpb-test-jur.tex
+#xelatex rpb-test-jur.tex
 
-xelatex rpb-test-medical.tex
-xelatex rpb-test-medical.tex
-xelatex rpb-test-medical.tex
-xelatex rpb-test-medical.tex
+#xelatex rpb-test-medical.tex
 
 # do we need this???
 #xelatex rpb-test-mpfr.tex
-#xelatex rpb-test-mpfr.tex
 
-xelatex rpb-test-multi.tex
-xelatex rpb-test-multi.tex
-xelatex rpb-test-multi.tex
-xelatex rpb-test-multi.tex
+#xelatex rpb-test-multi.tex
 
-xelatex rpb-test-short_1d21.tex
-xelatex rpb-test-short_1d21.tex
-xelatex rpb-test-short_1d21.tex
-xelatex rpb-test-short_1d21.tex
+#xelatex rpb-test-short_1d21.tex
