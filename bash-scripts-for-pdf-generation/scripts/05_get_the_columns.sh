b541ab5102c4ee6aa6e51c1eda284a64e3d5cd21
michal-fre <mike@mbp.local>
2015-09-24 11:10:15 +0200

    current dataset

--- bash-scripts-for-pdf-generation/scripts/05_get_the_columns.sh
+++ bash-scripts-for-pdf-generation/scripts/05_get_the_columns.sh
@@ -21,8 +21,8 @@ cat ../temp/mpfr.data           |cut -f 1      > ../temp/mpfr.h.column
 cat ../temp/medical.data 	|cut -f 1,2,3,7,10   	> ../temp/medical.d.column
 cat ../temp/medical.headings 	|cut -f 1,2,3,7,10	 	> ../temp/medical.h.column
 
-cat ../temp/multi.data 		|cut -f 2,3,6,18,24,44 	> ../temp/multi.d.column
-cat ../temp/multi.headings 	|cut -f 2,3,6,18,24,44 	> ../temp/multi.h.column
+cat ../temp/multi.data 		|cut -f 2,3,6,18 	> ../temp/multi.d.column
+cat ../temp/multi.headings 	|cut -f 2,3,6,18 	> ../temp/multi.h.column
 
 cat ../temp/short_1d21.data 	|cut -f 1,2,3,5,8,18   	> ../temp/short_1d21.d.column
 cat ../temp/short_1d21.headings |cut -f 1,2,3,5,8,18 	> ../temp/short_1d21.h.column
