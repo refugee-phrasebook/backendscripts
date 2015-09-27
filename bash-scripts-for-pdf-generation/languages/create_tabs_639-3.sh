5580ee5f898c53d09a0b28b87a24c45a0ad7ce7d
michal-fre <mike@mbp.local>
2015-09-27 14:55:04 +0200

    lists with languages

--- bash-scripts-for-pdf-generation/languages/create_tabs_639-3.sh
+++ bash-scripts-for-pdf-generation/languages/create_tabs_639-3.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cat iso-639-3.tab |cut -f 1,7 > iso-639-3_Id-Ref_Name.tab
+cat iso-639-3.tab |tail -n +2|cut -f 1,7 > iso-639-3_Id-Ref_Name.tab
 
 sort -k 2 iso-639-3-macrolanguages.tab |cut -f1,2|tail -n +2 > iso-639-3-macrolanguages_sort_by_micro.tab
