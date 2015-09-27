0690d21b0d096076081ae3254b182c7ec6ec8af8
michal-fre <mike@mbp.local>
2015-09-27 15:17:32 +0200

    list of macro languages

--- bash-scripts-for-pdf-generation/languages/create_tabs_639-3.sh
+++ bash-scripts-for-pdf-generation/languages/create_tabs_639-3.sh
@@ -2,4 +2,5 @@
 
 cat iso-639-3.tab |tail -n +2|cut -f 1,7 > iso-639-3_Id-Ref_Name.tab
 
-sort -k 2 iso-639-3-macrolanguages.tab |cut -f1,2|tail -n +2 > iso-639-3-macrolanguages_sort_by_micro.tab
+tail -n +2 iso-639-3-macrolanguages.tab |sort -k 2 |cut -f 1,2 > iso-639-3-macrolanguages_sort_by_micro.tab
+tail -n +2 iso-639-3-macrolanguages.tab | cut -f 1 |sort | uniq> iso-639-3-macrolist.tab
