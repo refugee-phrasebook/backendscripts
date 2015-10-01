73a1505cb89870731e53e237e743da2191f44c11
michal-fre <mike@mbp.local>
2015-10-01 22:34:33 +0200

    quickfix

--- bash-scripts-for-pdf-generation/scripts/04_cut_the_first_lines.sh
+++ bash-scripts-for-pdf-generation/scripts/04_cut_the_first_lines.sh
@@ -9,14 +9,16 @@ sed '/^[[:space:]]*$/d' ../download/tsv/medical.tsv 	> ../temp/medical.tsv2
 sed '/^[[:space:]]*$/d' ../download/tsv/mpfr.tsv 	> ../temp/mpfr.tsv2
 sed '/^[[:space:]]*$/d' ../download/tsv/multi.tsv	> ../temp/multi.tsv2
 sed '/^[[:space:]]*$/d' ../download/tsv/short_1d21.tsv	> ../temp/short_1d21.tsv2
+sed '/^[[:space:]]*$/d' ../download/tsv/short_1pwmnm.tsv	> ../temp/short_1pwmnm.tsv2
 
 # striptease
-
+# cut unneeded comments in first lines
 tail -n +2 ../temp/jur.tsv2      > ../temp/jur.tail
 tail -n +3 ../temp/medical.tsv2  > ../temp/medical.tail
 tail -n +2 ../temp/mpfr.tsv2     > ../temp/mpfr.tail
 tail -n +2 ../temp/multi.tsv2    > ../temp/multi.tail
 tail -n +1 ../temp/short_1d21.tsv2 > ../temp/short_1d21.tail
+tail -n +1 ../temp/short_1pwmnm.tsv2 > ../temp/short_1pwmnm.tail
 #tail -n +3 ../temp/short_1d21.tsv2 > ../temp/short_1ipk.tail
 
 echo "cut the headings"
@@ -25,6 +27,7 @@ head -1 ../temp/medical.tail  > ../temp/medical.headings
 head -1 ../temp/mpfr.tail     > ../temp/mpfr.headings
 head -1 ../temp/multi.tail    > ../temp/multi.headings
 head -1 ../temp/short_1d21.tail > ../temp/short_1d21.headings
+head -1 ../temp/short_1pwmnm.tail > ../temp/short_1pwmnm.headings
 #head -1 ../temp/short_1d21.tail > ../temp/short_1ipk.headings
 
 echo "extract rest of data"
@@ -33,4 +36,5 @@ tail -n +2 ../temp/medical.tail  > ../temp/medical.data
 tail -n +2 ../temp/mpfr.tail     > ../temp/mpfr.data
 tail -n +2 ../temp/multi.tail    > ../temp/multi.data
 tail -n +2 ../temp/short_1d21.tail > ../temp/short_1d21.data
+tail -n +4 ../temp/short_1pwmnm.tail > ../temp/short_1pwmnm.data
 #tail -n +2 ../temp/short_1d21.tail > ../temp/short_1ipk.data
