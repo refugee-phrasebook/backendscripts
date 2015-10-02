8cfe536cbbfea7ed0c3cca8d033713413739c2fb
michal-fre <mike@mbp.local>
2015-10-02 09:19:24 +0200

    clean up counting

--- bash-scripts-for-pdf-generation/scripts/02_row_and_language.sh
+++ bash-scripts-for-pdf-generation/scripts/02_row_and_language.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
 echo "this is the juridical"
-head -2 ../download/tsv/jur.tsv  |tail -1  |tr -s '\t' '\n'|nl -b a
+#head -2 ../download/tsv/jur.tsv |tail -1 | tr -s '\t' '\n' | nl -b a
+awk 'NR==2' ../download/tsv/jur.tsv | tr '\t' '\n'|nl -b a
+awk 'NR==2' ../download/tsv/jur.tsv | tr '\t' '\n'| wc -l
 
 echo "this is the short 1d21"
-head -1 ../download/tsv/short_1d21.tsv  |tail -1  |tr -s '\t' '\n'|nl -b a
-
-#echo "this is the short 1d21"
-#head -3 ../download/tsv/short_1ipk.tsv  |tail -1  |tr -s '\t' '\n'|nl -b a
+#head -1 ../download/tsv/short_1d21.tsv |tail -1 |tr -s '\t' '\n'|nl -b a
+awk 'NR==1' ../download/tsv/short_1d21.tsv | tr '\t' '\n' | nl -b a
+awk 'NR==1' ../download/tsv/short_1d21.tsv | tr '\t' '\n' | wc -l
 
 echo "this is the medical"
-head -3 ../download/tsv/medical.tsv  |tail -1  |tr -s '\t' '\n'|nl -b a
+#head -3 ../download/tsv/medical.tsv |tail -1 |tr -s '\t' '\n'|nl -b a
+awk 'NR==3' ../download/tsv/medical.tsv | tr '\t' '\n'|nl -b a
+awk 'NR==3' ../download/tsv/medical.tsv | tr '\t' '\n'|wc -l
 
 echo "this is the medical - MPfR"
-head -3 ../download/tsv/mpfr.tsv  |tail -1  |tr -s '\t' '\n'|nl -b a
+#head -3 ../download/tsv/mpfr.tsv |tail -1 |tr -s '\t' '\n'|nl -b a
+awk 'NR==3' ../download/tsv/mpfr.tsv | tr '\t' '\n'|nl -b a
+awk 'NR==3' ../download/tsv/mpfr.tsv | tr '\t' '\n'|wc -l
 
 echo "this is the multi"
-head -2 ../download/tsv/multi.tsv  |tail -1  |tr -s '\t' '\n'|nl -b a
-echo "row 11???"
+#head -2 ../download/tsv/multi.tsv |tail -1 |tr -s '\t' '\n'|nl -b a
+#echo "row 11???"
+
+awk 'NR==2'  ../download/tsv/multi.tsv | tr '\t' '\n'|nl -b a
+awk 'NR==2' ../download/tsv/multi.tsv | tr '\t' '\n'|wc -l
 
 echo "keep it as a reference as you will need it for selecting rows"
 
 echo "BETA this is a quickfix short 1pwmnm"
-sed '1q' ../download/tsv/short_1pwmnm.tsv |tr -s '\t' '\n'|nl -b a
+awk 'NR==1'  ../download/tsv/short_1pwmnm.tsv|tr '\t' '\n'|nl -b a
+awk 'NR==1'  ../download/tsv/short_1pwmnm.tsv|tr '\t' '\n'|wc -l
--- bash-scripts-for-pdf-generation/scripts/05_get_the_columns.sh
+++ bash-scripts-for-pdf-generation/scripts/05_get_the_columns.sh
@@ -26,3 +26,45 @@ cat ../temp/multi.headings 	|cut -f 2,4,6,7,10 	> ../temp/multi.h.column
 
 cat ../temp/short_1d21.data 	|cut -f 1,2,3,5,8,18   	> ../temp/short_1d21.d.column
 cat ../temp/short_1d21.headings |cut -f 1,2,3,5,8,18 	> ../temp/short_1d21.h.column
+
+
+# starting with beta
+# 
+#
+# get the numbers of columns 
+LANGUAGE_LINE_SHORT_1pwmnm=`sed '1q' ../download/tsv/short_1pwmnm.tsv`
+echo $LANGUAGE_LINE_SHORT_1pwmnm
+LINEUP_LANGUAGES_SHORT=`echo $LANGUAGE_LINE_SHORT_1pwmnm |sed 's/$(printf '\t')"/\n' |nl -b a`
+echo $LINEUP_LANGUAGES_SHORT
+COUNT_COLUMNS_short_1pwmnm=`echo $LANGUAGE_LINE_SHORT_1pwmnm |tr -s '[:space:]' '\n' |wc -l`
+echo "we have $COUNT_COLUMNS_short_1pwmnm columns in short_1pwmnm.tsv"
+
+
+# catch duplicates
+#tr using space
+DUPLICATES_SHORT=`sed '1q' ../download/tsv/short_1pwmnm.tsv |tr -s '[:space:]' '\n'|sort|uniq -d`
+
+if [ "$DUPLICATES_SHORT" != "" ]; then
+	echo "WARNING: we have duplicate language names!!!"
+	# todo: show which ones/which columns
+	echo duplicate language: $DUPLICATES_SHORT
+	echo "the columns are in"
+
+else
+	echo "good: we have no duplicate languages"
+fi
+
+
+# strip
+for (( col=1; col<=$COUNT_COLUMNS_short_1pwmnm;col++))
+do
+	echo "working on $col"
+	language=`cut -f $col ../temp/short_1pwmnm.headings`
+	# catch all empty languages
+	if [ "$language" = ""	]; then
+		echo "WARNING WARNING WARNING: Column $col has no language set"
+		echo "we will use this as a filename: ../temp/short_1pwmnm.headings."$col
+	else
+	echo "use this one ../temp/short_1pwmnm.headings."$language
+	fi
+done
